dbc45d323c0977eca9a022b405a9364a93c032cd
cbdemoraes <cbgabriel2016@gmail.com>
2021-08-29 02:47:53 -0400

    passando uma função para a função .mao

---
+++ funcoes/callback_3.js
@@ -0,0 +1,24 @@
+const nums = [1,2,3,4,5]
+
+// const dobro = (n,i,a) => n * 2 + i + a.length
+const dobro = n => n * 2
+console.log(nums.map(dobro))
+
+
+const nomes = ['Gabriel','Lucia','Kleber','Natalia']
+const primeiraLetra = texto => texto[0]
+const letras = nomes.map(primeiraLetra)
+console.log(nomes,letras)
+
+const carrinho = [
+    { nome: 'Caneta', qtde: 0, preco: 7.99},
+    { nome: 'Impressora', qtde: 2, preco: 649.50},
+    { nome: 'Caderno', qtde:4, preco: 27.10},
+    { nome: 'Lapis', qtde:3, preco: 5.82},
+    { nome: 'Tesoura', qtde:1, preco: 19.20},
+]
+
+const getNome = carrinho => carrinho.nome
+console.log(carrinho.map(getNome))
+const total = carrinho => carrinho.qtde * carrinho.preco
+console.log(carrinho.map(total))
