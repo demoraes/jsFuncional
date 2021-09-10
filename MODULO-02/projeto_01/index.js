acf39dd6f83120e642d1e0627dd0dcf73a5694b5
cbdemoraes <cbgabriel2016@gmail.com>
2021-09-09 23:50:45 -0400

    usando promisse para ler os arquivos

--- MODULO-02/projeto_01/index.js
+++ MODULO-02/projeto_01/index.js
@@ -3,5 +3,5 @@ const fn = require('./funcoes')
 
 const caminho = path.join(__dirname, '..', 'dados', 'legendas')
 
-const arquivos = fn.lerDiretorio(caminho)
-console.log(arquivos)
+fn.lerDiretorio(caminho)
+    .then(console.log)
