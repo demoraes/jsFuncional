03613bd0b9d4d618adbf23e9c22129fa917abcf3
cbdemoraes <cbgabriel2016@gmail.com>
2021-10-06 18:24:47 -0400

    operadores #03

---
+++ MODULO-05/rxjs/operadores_5.js
@@ -0,0 +1,25 @@
+const { of, Observable } = require('rxjs')
+
+function terminadoCom(parteFinal) {
+    return function (fonte) {
+        return Observable.create(subscriber => {
+            fonte.subscribe({
+                next(texto) {
+                    if (texto.endsWith(parteFinal)) {
+                        subscriber.next(texto)
+                    }
+                },
+                error(e) {
+                    subscriber.error(e)
+                },
+                complete() {
+                    subscriber.complete()
+                }
+            })
+        })
+    }
+}
+
+of('Ana Silva', 'Maria Silva', 'Pedro Rocha')
+    .pipe(terminadoCom('Silva'))
+    .subscribe(console.log)
