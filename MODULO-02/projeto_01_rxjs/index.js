4cd6a82393b798311194ce9c17fa819142e98bf9
cbdemoraes <cbgabriel2016@gmail.com>
2021-10-13 01:43:39 -0400

    lendo diretorios

---
+++ MODULO-02/projeto_01_rxjs/index.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const fn = require('./funcoes')
+
+const caminho = path.join(__dirname, '..', 'dados', 'legendas')
+
+// const simbolos = [
+//     '.', '?', '-', ',', '"', '_',
+//     '<i>', '</i>', '\r', '[', ']',
+//     '(', ')', '♪'
+// ]
+
+fn.lerDiretorio(caminho)
+    .subscribe(console.log)
+
+// fn.lerDiretorio(caminho)
+//     .then(fn.elementorTerminadosCom('.srt'))
+//     .then(fn.lerArquivos)
+//     .then(fn.mesclarElementos)
+//     .then(fn.separarTextoPor('\n'))
+//     .then(fn.removerElementosVazio)
+//     .then(fn.removerElementosSeIncluir('-->'))
+//     .then(fn.removerElementosSeApenasNumero)
+//     .then(fn.removerSimbolos(simbolos))
+//     .then(fn.mesclarElementos)
+//     .then(fn.separarTextoPor(' '))
+//     .then(fn.removerElementosVazio)
+//     .then(fn.agruparElementos)
+//     .then(fn.ordenarPorAtribNumerico('qtde', 'desc'))
+//     .then(console.log)
